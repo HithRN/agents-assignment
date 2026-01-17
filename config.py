858ed9422fc41190c968b44d3c9d552a21f2391b
HithRN <hithnidhan@gmail.com>
2026-01-17 16:50:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-import os
+#import os
 
-SOFT_TOKENS = os.getenv("SOFT_TOKENS", "yeah,ok,hmm,uh-huh,right,mm-hmm,aha")
-HARD_TOKENS = os.getenv("HARD_TOKENS", "stop,wait,no,hold,pause,hey")
-MAX_SOFT_LENGTH = int(os.getenv("MAX_SOFT_LENGTH", "3"))
+#SOFT_TOKENS = os.getenv("SOFT_TOKENS", "yeah,ok,hmm,uh-huh,right,mm-hmm,aha")
+#HARD_TOKENS = os.getenv("HARD_TOKENS", "stop,wait,no,hold,pause,hey")
+#MAX_SOFT_LENGTH = int(os.getenv("MAX_SOFT_LENGTH", "3"))
